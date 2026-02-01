d684a65ff34a910a2f8a28d9b096841404b68aae
abinaya210894-ccc <abinaya210894@gmail.com>
2026-02-01 16:31:11 +0530

    Update demoqaLogin.spec.js

--- e2e/demoqaLogin.spec.js
+++ e2e/demoqaLogin.spec.js
@@ -1,1 +1,7 @@
-
+import {test , expect} from '@playwright/test';
+test('demoqa , async ({page}) ==> {
+     await page.goto ('https://demoqa.com/');
+await page.waitfortimeout(2000); 
+await page.getbyplaceholder('Full Name').fill('Abinaya');
+await page.getbyplaceholder('name@example.com').fill('abc@gmail.com');
+});
