54795676345afc179be27bb11da7fc74c0c1f5f8
abinaya210894-ccc <abinaya210894@gmail.com>
2026-02-08 18:11:49 +0530

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,11 +1,16 @@
-import {test , expect} from '@playwright/test';
-test ('Login', async({page}) =>{
-await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-await page.getByPlaceholder('username').fill('Admin');
-await page.waitForTimeout(2000);
-await page.getByPlaceholder('Password').fill('admin123'); 
-await page.waitForTimeout(2000);
-await page.getByRole('button', { name: 'Login' }).click();
-await page.waitForTimeout(10000);
+import {test , expect} from 'playwright/test';
+test('Login' , async ({page}) => {
+    await page.goto ('https://www.irctc.co.in/nget/train-search');
+    await page.getByRole('searchbox' , {name :'Enter From station. Input is Mandatory.'}).fill('Madurai');
+    await page. getByText(' MADURAI JN - MDU ').click();
+    await page. getByRole('searchbox' , {name: 'Enter To station. Input is Mandatory.'}). fill('Chennai');
+    await page. getByText (' CHENNAI EGMORE - MS ') .click();
+    await page. locator('[type="text"]') . click();
+    await page. getByText ('9') .click();
+    await page. getByText ('All Classes') . selectOption ('AC First Class (1A) '); 
+
+
+
+
 
 });
